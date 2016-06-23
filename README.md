24c8dbeb75c71ed8e45863d472ff57567fb8539c
Mishk0 <mshelemetev@gmail.com>
2016-06-22 21:37:34 -0400

    Let the fun begin

--- README.md
+++
@@ -1,2 +0,0 @@
-# pythonVideoStream
-what I want to do here is just some experiments with ability to push video stream from opencv to remote socket, so that would have server and client sides
---
+++ client/client.py
@@ -0,0 +1,25 @@
+
+class Client:
+
+    def __init__(self,conf: dict):
+        #setup socket
+        self.socket = self.setup_socket(conf)
+
+    def send_handshake(self):
+        pass
+
+    def initiate_stream(self):
+        pass
+
+    def push_stream(self):
+        pass
+
+    def encrypt_stream(self):
+        pass
+
+    def compress_stream(self):
+        pass
+
+    def setup_socket(self,conf:dict):
+        #returns socket
+        pass
